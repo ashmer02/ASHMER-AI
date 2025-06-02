311f3b57f7193e5a82e9fa58a8673e57c0ed0427
ashmer02 <ashmerworld@gmail.com>
2025-06-03 03:48:31 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+fetch('https://ashmer-ai-test2.ashutoshmerwade-pro.workers.dev/', {
+  method: 'POST',
+  body: JSON.stringify({ prompt: "Hello AI" }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+.then(res => res.json())
+.then(data => {
+  console.log(data.response); // Display it in chatbot
+});
